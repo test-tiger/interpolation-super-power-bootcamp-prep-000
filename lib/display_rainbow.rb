9c545e85a8720e31dfba4aea15048b51e9b5887d
alluring-recipe-9997 <bhardwaj.10@gmail.com>
2019-08-18 08:02:49 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,4 @@
 # Write your #display_rainbow method here
 def display_rainbow(colors=[])
-  
+  puts "R: #{col[0]}, O: #{col[1]}, Y: #{col[2]}, G: #{col[3]}, B: #{col[4]}, I: #{col[5]}, V: #{col[6]}"
 end
